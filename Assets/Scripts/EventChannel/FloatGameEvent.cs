b292431e4cd67270b598b07a42727cbbb7532a2b
hqahaad <grabq04@gmail.com>
2025-03-26 09:46:53 +0900

    [Chore] 스프라이트 에셋

--- Assets/Scripts/EventChannel/FloatGameEvent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/Event/Float")]
-public class FloatGameEvent : GameEvent<float>
-{
-
-}
